5c2f0261391a69b8bfa073600ab35c1441c3d828
Dave Neve <toucheturtle84@gmail.com>
2025-10-28 07:42:27 +0000

    created database models

---
+++ Data/DTOs/CalendarDTOs/EventRecordDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PortfolioBuilder.Data.DTOs.CalendarDTOs
+{
+    public record EventRecordDTO
+        (
+        Guid Id,
+        string title,
+        DateTime startTime,
+        DateTime? endTime,
+        string? details
+        );
+}
---
+++ Data/Models/EventRecord.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PortfolioBuilder.Data.Models
+{
+    public class EventRecord
+    {
+        public Guid Id { get; set; }
+
+        [Required]
+        [MaxLength(150)]
+        public string title { get; set; }
+
+        [Required]
+        public DateTime startTime { get; set; }
+
+        public DateTime? endTime { get; set; }
+
+        public string? details { get; set; }
+
+    }
+}
